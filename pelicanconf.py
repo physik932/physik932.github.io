5f6767234352940013f2424d3a13ba4496023240
physik932 <physik932@gmail.com>
2020-06-07 21:55:08 -0600

    new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Chosen theme from pelican-themes
+THEME = "/Users/rishi.sheth/p/pelican-themes/storm"
